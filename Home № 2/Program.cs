b187d5bc6819a47d5d583811c83b078e6a38208e
Kalininskiy Andrei <kresthaus@mail.ru>
2022-07-04 19:43:36 +0400

    Готова

--- Home № 2/Program.cs
+++ Home № 2/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+
+
+Console.Clear();
+
+Console.Write("Введите строку:");
+int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+Console.Write("Введите столбец:");
+int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+
+int n = 3; // размер массива
+int m = 4; // размер массива
+Random random = new Random();
+int[,] arr = new int[n, m];
+
+Console.WriteLine("Исходный массив:");
+  for (int i = 0; i < arr.GetLength(0); i++)
+    {
+      for (int j = 0; j < arr.GetLength(1); j++)
+        {
+          arr[i, j] = random.Next(10,99);
+          Console.Write("{0} ", arr[i, j]);
+        }
+Console.WriteLine();
+}
+    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+{
+Console.WriteLine("Элемент не существует");
+}
+    else
+{
+Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+}
+Console.ReadLine();
